f12d96d0e9eb5bc481f7a8c4dd92c6f88f70a258
Landy Schlebach <landy_2093@hotmail.com>
2016-10-11 18:34:50 -0500

    ADD: Initial business logic of project

--- src/main/java/com/db/sysgob/bo/ProjectBO.java
+++ src/main/java/com/db/sysgob/bo/ProjectBO.java
@@ -1,5 +1,41 @@
 package com.db.sysgob.bo;
 
-public class ProjectBO {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.db.sysgob.entity.Category;
+import com.db.sysgob.entity.Classification;
+import com.db.sysgob.entity.Project;
+import com.db.sysgob.repository.CategoryRepository;
+import com.db.sysgob.repository.ProjectRepository;
+
+@Component
+public class ProjectBO {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public Long calculateCategory(Classification classification) {
+		Long total = classification.getRiskClassification() + 
+				classification.getOthersClassification() + 
+				classification.getFinanceClassification() + 
+				classification.getOtherImplications() + 
+				classification.getStrategicClassification();
+		
+		return total;
+	}
+	
+	public Project defineCategory(Project project, Long points) {
+		Long systemCategoryId = 0L;
+		Category category = categoryRepository.getById(project.getCategoryId());
+		Long averageCategory = (category.getMinScore() + points) / 2;
+		
+		systemCategoryId = categoryRepository.getCategoryByPoints(averageCategory);
+		project.setCategoryId(systemCategoryId);
+		
+		return project;
+	}
 }
--- src/main/java/com/db/sysgob/bo/UserBO.java
+++ src/main/java/com/db/sysgob/bo/UserBO.java
@@ -1,5 +1,45 @@
 package com.db.sysgob.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.db.sysgob.entity.Role;
+import com.db.sysgob.entity.User;
+import com.db.sysgob.entity.UserBasicInfo;
+import com.db.sysgob.repository.RoleRepository;
+import com.db.sysgob.repository.UserRepository;
+
 public class UserBO {
 
+	private final String ADVANCED_USER = "ADVANCED_USER";
+	private final String PM_USER = "PM_USER";
+	private final String ACCOUNTANT_USER = "ACCOUNTANT_USER";
+	private final String MANAGER_USER = "MANAGER_USER"; 
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public String getView(UserBasicInfo user) {
+		Role role = roleRepository.getById(user.getRoleId());
+		
+		return "";
+	}
+	
+	public void blockUser(User user) {
+		try {
+		user.blocked(true);
+		userRepository.updateUser(user);
+		} catch(Exception e) {
+			e.getMessage();
+		}
+	}
+	
+	public Long getDependencyOfUser(Long userId) {
+		User user = userRepository.getUserById(userId);
+		Role role = roleRepository.getById(user.getRoleId());
+		
+		return role.getDependencyId();
+	}
 }
